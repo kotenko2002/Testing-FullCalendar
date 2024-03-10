2bcc020054b59a4abcb20a13fa2d87220f0d2a42
kotenko2002 <kotenko2002@gmail.com>
2024-03-10 18:03:06 +0200

    add comments

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,19 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import './App.css';
 import ukLocale from '@fullcalendar/core/locales/uk';
 import { generateEvents, Event  } from './generateEvents';
+import { DatesSetArg } from '@fullcalendar/core';
 
 const App: React.FC = () => {
+  /*
+    По суті костомні хендлери на кнопки не потрібні,
+    зрозумів це після того як побачив datesSet
+
+    Не буду видалять, хай будуть як приклад
+  */
+  /*
+    Кст з datesSet є проблемка,
+    при першому рендері - функція викликається двічі
+  */
   const [events, setEvents] = useState<Event[]>([]);
   const calendarRef = useRef<FullCalendar>(null);
   
@@ -16,27 +27,16 @@ const App: React.FC = () => {
   }, []);
 
   const handleOnTodayButtonClick = () => {
-    console.log('today clicked!');
-
-    if (calendarRef.current) {
-      calendarRef.current.getApi().today();
-    }
+    //custom action
+    if (calendarRef.current) calendarRef.current.getApi().today();
   }
-
   const handleOnPrevButtonClick = () => {
-    console.log('prev clicked!');
-
-    if (calendarRef.current) {
-      calendarRef.current.getApi().prev();
-    }
+    //custom action
+    if (calendarRef.current) calendarRef.current.getApi().prev();
   }
-
-  const handleOnNextButtonClick = () => {
-    console.log('next clicked!');
-
-    if (calendarRef.current) {
-      calendarRef.current.getApi().next();
-    }
+  const handleOnNextButtonClick = (ev: MouseEvent, element: HTMLElement) => {
+    //custom action
+    if (calendarRef.current) calendarRef.current.getApi().next();
   }
   
   return (
@@ -50,12 +50,16 @@ const App: React.FC = () => {
         slotLabelFormat={{ hour: '2-digit', minute: '2-digit', hour12: false }} // 1PM -> 13:00 
         locale={ukLocale} // 24:00 -> 0:00 + локалізація
         firstDay={1} // робить Пон першим днем
-        dayHeaderFormat={{ weekday: 'long', day: '2-digit' }} // Mon 17.03 -> Mon 17
+        dayHeaderFormat={{ weekday: 'short', day: '2-digit' }} // Mon 17.03 -> Mon 17
         contentHeight="auto" // прибирає скрол
         customButtons={{
           prev: { click: handleOnPrevButtonClick },
           next: { click: handleOnNextButtonClick },
           today: { text: "Сьогодні", click: handleOnTodayButtonClick }
+        }}
+        datesSet={(arg: DatesSetArg) => {
+          console.log(`[${arg.startStr} - ${arg.endStr}]`);
+          setEvents(generateEvents());
         }}
         eventClick={info => alert(`Ви натиснули на подію: ${info.event.title}\n Початок: ${info.event.start}\n Кінець: ${info.event.end}`)}
       />
